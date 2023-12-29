b770da17bb31f35bc228be27e29b56e43b7fe96a
brennop <brennopcordeiro@gmail.com>
2023-12-29 18:02:41 -0300

    first instruction (untested)

--- include/cpu.h
+++ include/cpu.h
@@ -1,31 +1,27 @@
 #ifndef __CPU_H__
 #define __CPU_H__
 
-#include "common.h"
 #include "bus.h"
+#include "common.h"
 
 typedef enum {
-  CARRY_FLAG = 0x01,
-  ZERO_FLAG = 0x02,
-  INTERRUPT_DISABLE_FLAG = 0x04,
-  DECIMAL_MODE_FLAG = 0x08, // unused in NES
-  BREAK_FLAG = 0x10,
-  UNUSED_FLAG = 0x20,
-  OVERFLOW_FLAG = 0x40,
-  NEGATIVE_FLAG = 0x80
+  FLAG_CARRY = 0x01,
+  FLAG_ZERO = 0x02,
+  FLAG_INTERRUPT_DISABLE = 0x04,
+  FLAG_DECIMAL_MODE = 0x08, // unused in NES
+  FLAG_BREAK = 0x10,
+  FLAG_UNUSED = 0x20,
+  FLAG_OVERFLOW = 0x40,
+  FLAG_NEGATIVE = 0x80
 } StatusFlag;
 
 typedef struct {
-  uint8_t a; // accumulator
-  uint8_t x; // x register
-  uint8_t y; // y register
-  uint8_t sp; // stack pointer
-  uint16_t pc; // program counter
+  uint8_t a;      // accumulator
+  uint8_t x;      // x register
+  uint8_t y;      // y register
+  uint8_t sp;     // stack pointer
+  uint16_t pc;    // program counter
   uint8_t status; // status register
-
-  uint8_t fetched; // fetched data
-  uint16_t addr_abs; // absolute address
-  uint16_t addr_rel; // relative address
 
   int cycles;
 
--- src/cpu.c
+++ src/cpu.c
@@ -1,94 +1,8 @@
 #include "cpu.h"
 #include "instructions.h"
 
-uint8_t cpu_fetch_data(CPU *cpu, AddressingMode addressing_mode) {
-  switch (addressing_mode) {
-  case ADDR_MODE_IMP:
-    cpu->fetched = cpu->a;
-    return 0;
-  case ADDR_MODE_IMM:
-    cpu->addr_abs = cpu->pc++;
-    return 0;
-  case ADDR_MODE_ZP0:
-    cpu->addr_abs = bus_read(cpu->bus, cpu->pc++, false);
-    cpu->addr_abs &= 0x00FF;
-    return 0;
-  case ADDR_MODE_ZPX:
-    cpu->addr_abs = bus_read(cpu->bus, cpu->pc++, false) + cpu->x;
-    cpu->addr_abs &= 0x00FF;
-    return 0;
-  case ADDR_MODE_ZPY:
-    cpu->addr_abs = bus_read(cpu->bus, cpu->pc++, false) + cpu->y;
-    cpu->addr_abs &= 0x00FF;
-    return 0;
-  case ADDR_MODE_ABS:
-    cpu->addr_abs = bus_read(cpu->bus, cpu->pc++, false);
-    cpu->addr_abs |= (bus_read(cpu->bus, cpu->pc++, false) << 8);
-    return 0;
-  case ADDR_MODE_ABX: {
-    cpu->addr_abs = bus_read(cpu->bus, cpu->pc++, false);
-    uint16_t addr_abs_hi = bus_read(cpu->bus, cpu->pc++, false);
-    cpu->addr_abs |= (addr_abs_hi << 8);
-    cpu->addr_abs += cpu->x;
-
-    if ((cpu->addr_abs & 0xFF00) != (addr_abs_hi << 8)) {
-      return 1;
-    }
-    return 0;
-  }
-  case ADDR_MODE_ABY: {
-    cpu->addr_abs = bus_read(cpu->bus, cpu->pc++, false);
-    uint16_t addr_abs_hi = bus_read(cpu->bus, cpu->pc++, false);
-    cpu->addr_abs |= (addr_abs_hi << 8);
-    cpu->addr_abs += cpu->y;
-
-    if ((cpu->addr_abs & 0xFF00) != (addr_abs_hi << 8)) {
-      return 1;
-    }
-    return 0;
-  }
-  case ADDR_MODE_IND: {
-    uint16_t ptr_lo = bus_read(cpu->bus, cpu->pc++, false);
-    uint16_t ptr_hi = bus_read(cpu->bus, cpu->pc++, false);
-    uint16_t ptr = (ptr_hi << 8) | ptr_lo;
-
-    cpu->addr_abs = bus_read_wide(cpu->bus, ptr, false);
-
-    // TODO: check for page boundary crossing
-
-    return 0;
-  }
-  case ADDR_MODE_IZX: {
-    uint16_t t = bus_read(cpu->bus, cpu->pc++, false);
-    uint16_t lo =
-        bus_read(cpu->bus, (uint16_t)(t + (uint16_t)cpu->x) & 0x00FF, false);
-    uint16_t hi = bus_read(
-        cpu->bus, (uint16_t)(t + (uint16_t)cpu->x + 1) & 0x00FF, false);
-
-    cpu->addr_abs = (hi << 8) | lo;
-
-    return 0;
-  }
-  case ADDR_MODE_IZY: {
-    uint16_t t = bus_read(cpu->bus, cpu->pc++, false);
-    uint16_t lo = bus_read(cpu->bus, t & 0x00FF, false);
-    uint16_t hi = bus_read(cpu->bus, (t + 1) & 0x00FF, false);
-    cpu->addr_abs = (hi << 8) | lo;
-    cpu->addr_abs += cpu->y;
-
-    if ((cpu->addr_abs & 0xFF00) != (hi << 8)) {
-      return 1;
-    }
-    return 0;
-  }
-  case ADDR_MODE_REL: {
-    cpu->addr_rel = bus_read(cpu->bus, cpu->pc++, false);
-    if (cpu->addr_rel & 0x80) {
-      cpu->addr_rel |= 0xFF00;
-    }
-    return 0;
-  }
-  }
+static inline void cpu_set_flag(CPU *cpu, uint8_t offset, bool value) {
+  cpu->status |= (value << offset);
 }
 
 void cpu_init(CPU *cpu, Bus *bus) { cpu->bus = bus; }
@@ -99,10 +13,101 @@ void cpu_step(CPU *cpu) {
 
   cpu->pc++;
   uint8_t cycles = instruction.cycles;
+  uint8_t addressing_cycles = 0;
+  uint8_t operation_cycles = 0;
 
-  uint8_t memory_cycles = cpu_fetch_data(cpu, instruction.addressing_mode);
+  uint16_t addr;
+  uint8_t data;
 
-  cpu->cycles = cycles + memory_cycles;
+  // Addressing modes
+  switch (instruction.addressing_mode) {
+  case ADDR_MODE_IMP:
+    data = cpu->a;
+    break;
+  case ADDR_MODE_IMM:
+    addr = cpu->pc++;
+    break;
+  case ADDR_MODE_ZP0:
+    addr = bus_read(cpu->bus, cpu->pc++, false) & 0x00FF;
+    break;
+  case ADDR_MODE_ZPX:
+    addr = (bus_read(cpu->bus, cpu->pc++, false) + cpu->x) & 0x00FF;
+    break;
+  case ADDR_MODE_ZPY:
+    addr = (bus_read(cpu->bus, cpu->pc++, false) + cpu->y) & 0x00FF;
+    break;
+  case ADDR_MODE_ABS:
+    addr = bus_read(cpu->bus, cpu->pc++, false) |
+           (bus_read(cpu->bus, cpu->pc++, false) << 8);
+    break;
+  case ADDR_MODE_ABX: {
+    addr = bus_read(cpu->bus, cpu->pc++, false);
+    uint16_t addr_abs_hi = bus_read(cpu->bus, cpu->pc++, false);
+    addr |= (addr_abs_hi << 8);
+    addr += cpu->x;
+
+    if ((addr & 0xFF00) != (addr_abs_hi << 8))
+      addressing_cycles++;
+  }
+  case ADDR_MODE_ABY: {
+    addr = bus_read(cpu->bus, cpu->pc++, false);
+    uint16_t addr_abs_hi = bus_read(cpu->bus, cpu->pc++, false);
+    addr |= (addr_abs_hi << 8);
+    addr += cpu->y;
+
+    if ((addr & 0xFF00) != (addr_abs_hi << 8))
+      addressing_cycles++;
+  }
+  case ADDR_MODE_IND: {
+    uint16_t ptr_lo = bus_read(cpu->bus, cpu->pc++, false);
+    uint16_t ptr_hi = bus_read(cpu->bus, cpu->pc++, false);
+    uint16_t ptr = (ptr_hi << 8) | ptr_lo;
+
+    addr = bus_read_wide(cpu->bus, ptr, false);
+
+    // TODO: check for page boundary crossing
+  }
+  case ADDR_MODE_IZX: {
+    uint16_t t = bus_read(cpu->bus, cpu->pc++, false);
+    uint16_t lo =
+        bus_read(cpu->bus, (uint16_t)(t + (uint16_t)cpu->x) & 0x00FF, false);
+    uint16_t hi = bus_read(
+        cpu->bus, (uint16_t)(t + (uint16_t)cpu->x + 1) & 0x00FF, false);
+
+    addr = (hi << 8) | lo;
+  }
+  case ADDR_MODE_IZY: {
+    uint16_t t = bus_read(cpu->bus, cpu->pc++, false);
+    uint16_t lo = bus_read(cpu->bus, t & 0x00FF, false);
+    uint16_t hi = bus_read(cpu->bus, (t + 1) & 0x00FF, false);
+    addr = (hi << 8) | lo;
+    addr += cpu->y;
+
+    if ((addr & 0xFF00) != (hi << 8))
+      addressing_cycles++;
+  }
+  case ADDR_MODE_REL: {
+    addr = bus_read(cpu->bus, cpu->pc++, false);
+    if (addr & 0x80) {
+      addr |= 0xFF00;
+    }
+  }
+  }
+
+  if (instruction.addressing_mode != ADDR_MODE_IMP) {
+    data = bus_read(cpu->bus, addr, false);
+  }
+
+  switch (opcode) {
+  // TODO: Use a macro to generate this
+  case 0x21: // AND (Indirect,X)
+    cpu->a &= data;
+    cpu_set_flag(cpu, FLAG_ZERO, cpu->a == 0x00);
+    cpu_set_flag(cpu, FLAG_NEGATIVE, cpu->a & 0x80);
+    operation_cycles = 1;
+  }
+
+  cpu->cycles = cycles + addressing_cycles & operation_cycles;
 }
 
 void cpu_reset(CPU *cpu);
